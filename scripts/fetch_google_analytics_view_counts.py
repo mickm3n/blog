2dc0d139f37ee3ab604a6257cd8a541f2f4efff6
Mick Zhuang <mickzhuang@gmail.com>
2024-07-09 19:47:10 +0800

    ✨ try to use github action to update top posts

---
+++ scripts/fetch_google_analytics_view_counts.py
@@ -0,0 +1,48 @@
+from google.analytics.data_v1beta import BetaAnalyticsDataClient
+from google.analytics.data_v1beta.types import (
+    DateRange,
+    Dimension,
+    Metric,
+    RunReportRequest,
+)
+import json
+import re
+
+PATH_FILTER_PATTERNS = [
+    re.compile("/blog/.+"),
+    re.compile("/reading-notes/.+"),
+    re.compile("/wisdom/.+/.+"),
+    re.compile("/about/"),
+    re.compile("/changelog/"),
+]
+
+
+def get_view_count_dict(property_id="278031395"):
+    """Runs a simple report on a Google Analytics 4 property."""
+    # Using a default constructor instructs the client to use the credentials
+    # specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
+    client = BetaAnalyticsDataClient()
+    view_count_dict = dict()
+
+    request = RunReportRequest(
+        property=f"properties/{property_id}",
+        dimensions=[Dimension(name="pagePath")],
+        metrics=[Metric(name="screenPageViews")],
+        date_ranges=[DateRange(start_date="2020-03-31", end_date="today")],
+        limit=250000,
+    )
+    response = client.run_report(request)
+
+    for row in response.rows:
+        dimension = row.dimension_values[0].value
+        metric = row.metric_values[0].value
+        if any([pattern.match(dimension) for pattern in PATH_FILTER_PATTERNS]):
+            view_count_dict[dimension] = metric
+
+    return view_count_dict
+
+
+if __name__ == "__main__":
+    view_count_dict = get_view_count_dict()
+    with open("static/data/view_counts.json", "w") as wf:
+        json.dump(view_count_dict, wf)
---
+++ scripts/update_top_posts.py
@@ -0,0 +1,36 @@
+import json
+import os
+import re
+
+SHOW_TOP_N_POSTS = 5
+TITLE_PATTERN = 'title = "(.*?)"'
+
+with open("static/data/view_counts.json", "r") as rf:
+    view_counts = json.load(rf)
+
+
+with open("content/_index.md.template", "r") as rf:
+    content = rf.read()
+
+top_posts = dict(
+    sorted(view_counts.items(), key=lambda item: int(item[1]), reverse=True)[
+        :SHOW_TOP_N_POSTS
+    ]
+)
+
+replace_str = ""
+for top_post, view_count in top_posts.items():
+    if os.path.exists("content" + top_post + "index.md"):
+        path = "content" + top_post + "index.md"
+    else:
+        path = "content" + re.sub(r"/$", ".md", top_post)
+    with open(path, "r") as rf:
+        title = re.findall(TITLE_PATTERN, rf.read())[0]
+    replace_str += (
+        f'* [{title}]({top_post}) <span class="view-count">({view_count})</span>\n'
+    )
+
+content = content.replace("<replace>", replace_str)
+
+with open("content/_index.md", "w") as wf:
+    wf.write(content)
