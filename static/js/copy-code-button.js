ccbc91b5f3539b1511e001b6383c8a0cc21a3899
Mick Zhuang <mickzhuang@gmail.com>
2024-07-28 15:52:41 +0800

    🐛 fix no line number case

--- static/js/copy-code-button.js
+++ static/js/copy-code-button.js
@@ -7,6 +7,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
         copyButton.innerText = 'Copy';
 
         copyButton.addEventListener('click', () => {
+            // When there is no line number, need to extract the code from the whole table
+            if (!codeBlock.querySelector('table tr')) {
+                navigator.clipboard.writeText(codeBlock.innerText).then(
+                    () => {
+                        copyButton.innerText = 'Copied!';
+                        setTimeout(() => {
+                            copyButton.innerText = 'Copy';
+                        }, 2000);
+                    },
+                    (error) => {
+                        console.error('Failed to copy code: ', error);
+                    }
+                );
+                return;
+            }
+
             // Get all the table rows
             const rows = codeBlock.querySelectorAll('table tr');
             // Extract only the text content from each row, ignoring the line numbers
